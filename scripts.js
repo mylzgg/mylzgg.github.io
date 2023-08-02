2c05530f515e20e5d4906a382b40dbf1b221f65f
Mylz Grantham <132848595+mylzgg@users.noreply.github.com>
2023-08-01 21:17:36 -0500

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -33,8 +33,61 @@ window.addEventListener('load', function () {
       }
     }
   })
+
+  var monke1 = document.getElementById('monke1')
+
+  monke1.addEventListener('click', function () {
+    // monke1ClickCount++
+
+    if (confirm('do you want to eat banan?')) {
+      alert('Here you go :)')
+      banan()
+    } else {
+      alert('im now sad monke')
+      sad()
+    }
+  })
+
+  var monke2 = document.getElementById('monke2')
+
+  monke2.addEventListener('click', function () {
+    // monke1ClickCount++
+
+    if (confirm('Are you a monke too?')) {
+      alert('yayyyy')
+      happyMonke()
+    } else {
+      alert('nnnnnoooooo')
+      sadMonke()
+    }
+  })
 })
+
 
 function rickroll() {
   window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
 }
+
+function sad() {
+  window.open('images/sad.webp')
+}
+
+function sadMonke() {
+  window.open('images/sad-monke.jpeg')
+}
+
+function banan() {
+  window.open('images/banan.gif')
+}
+
+function happyMonke() {
+  window.open('images/happy-monke.gif')
+}
+
+var bgColorChangeElements = document.querySelectorAll('[data-bg-color-change]')
+bgColorChangeElements.forEach(function (element) {
+  element.addEventListener('click', function (e) {
+    var color = e.currentTarget.getAttribute('data-bg-color-change')
+    document.body.style.backgroundColor = color
+  })
+})
