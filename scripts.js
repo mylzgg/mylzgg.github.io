26a1c59013e0e949fca0ed4fc843a93de0618aff
Mylz Grantham <132848595+mylzgg@users.noreply.github.com>
2023-10-20 17:11:40 -0500

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,5 +1,6 @@
 var carnageClickCount = 0
 var riotClickCount = 0
+var sounds = {}
 
 window.addEventListener('load', function () {
   var carnage = document.getElementById('carnage')
@@ -61,6 +62,42 @@ window.addEventListener('load', function () {
       sadMonke()
     }
   })
+
+  var smurfcat = document.getElementById('smurfcat')
+
+  smurfcat.addEventListener('click', function () {
+    if (confirm('YAY you have found the hidden smurf cat:o')) {
+      alert('now we are going to listen to some jams:)')
+      coolcat()
+    } else {
+      alert('why would you ignore  me? now you  have to pay')
+      angy()
+    }
+  })
+
+  var kitty = document.getElementById('kitty')
+
+  kitty.addEventListener('click', function () {
+    if (confirm('YAY you have found the hidden nyan cat cat:o')) {
+      alert('now were going to make you deaph:)')
+      meow()
+    } else {
+      alert('waa im sad now:(')
+      scream()
+    }
+  })
+
+  var cheese = document.getElementById('cheese')
+
+  cheese.addEventListener('click', function () {
+    if (confirm('YAY you have found the hidden cheese:o')) {
+      alert('now were going to make you say cheese:)')
+      cheeese()
+    } else {
+      alert('no cheese for you now:(')
+      mad()
+    }
+  })
 })
 
 
@@ -70,18 +107,63 @@ function rickroll() {
 
 function sad() {
   window.open('images/sad.webp')
+  playSound('sad.mp3')
+}
+
+function coolcat() {
+  window.open('images/coolcat.gif')
+  playSound('wel.mp3')
 }
 
 function sadMonke() {
   window.open('images/sad-monke.jpeg')
+    playSound('violen.mp3')
 }
 
 function banan() {
   window.open('images/banan.gif')
+  playSound('pnutbuter.mp3')
 }
 
 function happyMonke() {
   window.open('images/happy-monke.gif')
+  playSound('spinningmonke.mp3')
+}
+
+function angy() {
+  window.open('images/angy.webp')
+  playSound('doom.mp3')
+}
+
+function meow() {
+  window.open('images/wee.webp')
+  playSound('loud.mp3')
+}
+
+function scream() {
+  window.open('images/scream.gif')
+  playSound('aaaaaaaa.mp3')
+}
+
+function cheeese() {
+  window.open('images/cheese.gif')
+  playSound('dotdotdot.mp3')
+}
+
+function mad() {
+  window.open('images/no-cheese.jpeg')
+  playSound('aw-man.mp3')
+}
+
+function playSound(sound) {
+  var audio = sounds[sound]
+
+  if (!audio) {
+    sounds[sound] = new Audio('sounds/' + sound)
+    audio = sounds[sound]
+  }
+
+  audio.play()
 }
 
 var bgColorChangeElements = document.querySelectorAll('[data-bg-color-change]')
